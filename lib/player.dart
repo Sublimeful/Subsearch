8e3d3f3bdd19e31d32c92a62540c24c5ffd2bced
Sublimeful <Qiangwu2000@gmail.com>
2025-07-14 23:27:20 -0500

    Added a search page and search bar

---
+++ lib/player.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlayerPage extends StatefulWidget {
+  const PlayerPage({super.key});
+
+  @override
+  State<PlayerPage> createState() => _PlayerPageState();
+}
+
+class _PlayerPageState extends State<PlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("Hello player!")
+        ],
+      )
+    );
+  }
+}
---
+++ lib/search.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+      child: Column(
+        children: [
+          SearchBar(
+            padding: const WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 16.0)),
+            leading: Icon(Icons.search),
+            onSubmitted: (query) {
+              
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
